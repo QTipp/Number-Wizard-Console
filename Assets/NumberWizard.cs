f086ad1b1ed29e794c06f5a4039570f930fdbeac
Thomas Bathurst <26010356+QTipp@users.noreply.github.com>
2021-04-10 21:15:28 +0100

    Added player input

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -11,15 +11,29 @@ public class NumberWizard : MonoBehaviour
         int max = 1000;
         int min = 1;
 
+
         Debug.Log("Welcome to Number Wizard!");
         Debug.Log("Please pick a number! Don't tell me what it is...");
         Debug.Log("The highest number you can pick is: " + max + ".");
         Debug.Log("The lowest number you can pick is: " + min + ".");
+        Debug.Log("Tell me if your number is higher or lower than 500.");
+        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up Arrow key was pressed.");
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Down Arrow key was pressed.");
+        }    
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Enter was pressed!");
+        }
     }
 }
