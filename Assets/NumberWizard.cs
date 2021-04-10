b029372d4a61e4f52579535b3c651627baa35eef
Thomas Bathurst <26010356+QTipp@users.noreply.github.com>
2021-04-10 21:47:28 +0100

    Added guess variable and math

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -4,36 +4,39 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
-    void Start()
+  int max = 1000;
+  int min = 1;
+  int guess = 500;
+  // Start is called before the first frame update
+  void Start()
+  {
+    Debug.Log("Welcome to Number Wizard!");
+    Debug.Log("Please pick a number & don't tell me what it is...");
+    Debug.Log("The highest number you can pick is: " + max + ".");
+    Debug.Log("The lowest number you can pick is: " + min + ".");
+    Debug.Log("Tell me if your number is higher or lower than: " + guess + ".");
+    Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+    max = max + 1;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.UpArrow))
     {
-        int max = 1000;
-        int min = 1;
-
-
-        Debug.Log("Welcome to Number Wizard!");
-        Debug.Log("Please pick a number! Don't tell me what it is...");
-        Debug.Log("The highest number you can pick is: " + max + ".");
-        Debug.Log("The lowest number you can pick is: " + min + ".");
-        Debug.Log("Tell me if your number is higher or lower than 500.");
-        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+      min = guess;
+      guess = (max + min) / 2;
+      Debug.Log("Is it higher or lower than: " + guess + ".");
     }
-
-    // Update is called once per frame
-    void Update()
+    else if (Input.GetKeyDown(KeyCode.DownArrow))
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            Debug.Log("Up Arrow key was pressed.");
-        }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            Debug.Log("Down Arrow key was pressed.");
-        }    
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            Debug.Log("Enter was pressed!");
-        }
+      max = guess;
+      guess = (max + min) / 2;
+      Debug.Log("Is it higher or lower than: " + guess + ".");
     }
+    else if (Input.GetKeyDown(KeyCode.Return))
+    {
+      Debug.Log("Woohoo!");
+    }
+  }
 }
